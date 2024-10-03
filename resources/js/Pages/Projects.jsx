6100df53373e20513bef89f7fc0ac37960358a5d
Astt3r <alexmansharinganwolf@gmail.com>
2024-10-03 12:28:00 -0300

    xxxx

--- resources/js/Pages/Projects.jsx
+++ resources/js/Pages/Projects.jsx
@@ -1,27 +1,25 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
 export default function Dashboard() {
     return (
         <AuthenticatedLayout
             header={
-                <div className=''> 
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Proyectos
-                </h2>
-                <div className="flex justify-end">
-                <button  
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded align-right"
-        onClick={() => {/* Lógica para crear un nuevo proyecto */}}
-    >
-        Crear Nuevo Proyecto
-    </button>
+                <div className="">
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                        Proyectos
+                    </h2>
+                    <div className="flex justify-end">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded align-right"
+                            onClick={() => {
+                                /* Lógica para crear un nuevo proyecto */
+                            }}
+                        >
+                            Crear Nuevo Proyecto
+                        </button>
+                    </div>
                 </div>
-                </div>
-                
-                
-                
-                
             }
         >
             <Head title="Dashboard" />
@@ -31,38 +29,55 @@ export default function Dashboard() {
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             Vista Proyectos
-
-
-
                             <div id="content-wrapper">
-                                        <div className="section">
-                                            <h2 className="text-xl font-semibold">Bienvenido a la Plataforma de Gestión de Juntas de Vecinos</h2>
-                                            <p>Panel de Proyectos.</p>
-                                            <div className="quick-access grid grid-cols-2 gap-4">
-                                                <div className="bg-blue-500 p-4 text-white"><a href="#">Crear Proyecto</a></div>
-                                                
-                                            </div>
+                                <div className="section">
+                                    <h2 className="text-xl font-semibold">
+                                        Bienvenido a la Plataforma de Gestión de
+                                        Juntas de Vecinos
+                                    </h2>
+                                    <p>Panel de Proyectos.</p>
+                                    <div className="quick-access grid grid-cols-2 gap-4">
+                                        <div className="bg-blue-500 p-4 text-white">
+                                            <a href="#">Crear Proyecto</a>
                                         </div>
-
-                                        <div className="section my-6">
-                                            <h2 className="text-xl font-semibold">Proyectos Actuales</h2>
-                                            <div className="projects grid grid-cols-2 gap-4">
-                                                <div className="p-4 border">
-                                                    <h3 className="font-bold">Proyecto A</h3>
-                                                    <p>Descripción breve del proyecto.</p>
-                                                    <p><strong>Estado:</strong> En curso</p>
-                                                </div>
-                                                <div className="p-4 border">
-                                                    <h3 className="font-bold">Proyecto B</h3>
-                                                    <p>Descripción breve del proyecto.</p>
-                                                    <p><strong>Estado:</strong> Finalizado</p>
-                                                </div>
-                                            </div>
-                                            <a href="#" className="block mt-4">Ver Todos los Proyectos</a>
-                                        </div>
-
-                                        
                                     </div>
+                                </div>
+
+                                <div className="section my-6">
+                                    <h2 className="text-xl font-semibold">
+                                        Proyectos Actuales
+                                    </h2>
+                                    <div className="projects grid grid-cols-2 gap-4">
+                                        <div className="p-4 border">
+                                            <h3 className="font-bold">
+                                                Proyecto A
+                                            </h3>
+                                            <p>
+                                                Descripción breve del proyecto.
+                                            </p>
+                                            <p>
+                                                <strong>Estado:</strong> En
+                                                curso
+                                            </p>
+                                        </div>
+                                        <div className="p-4 border">
+                                            <h3 className="font-bold">
+                                                Proyecto B
+                                            </h3>
+                                            <p>
+                                                Descripción breve del proyecto.
+                                            </p>
+                                            <p>
+                                                <strong>Estado:</strong>{" "}
+                                                Finalizado
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <a href="#" className="block mt-4">
+                                        Ver Todos los Proyectos
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
