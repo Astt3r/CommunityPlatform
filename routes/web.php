3c6234cea873af35a6b6eda65c8a0cbc2cf931e3
Adolfo Luna <adolfo.luna@virginiogomez.cl>
2024-11-08 16:09:35 -0300

    style: borrado en lineas extras

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -33,26 +32,14 @@ Route::middleware('auth')->group(function () {
     })->name('projects.create');
 });
 
-
-
-
-
-
-
-
 Route::get('/finanzas', function () {
     return Inertia::render('Finanzas');
 })->name('finanzas');
-
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    
-
 });
 
 require __DIR__.'/auth.php';
